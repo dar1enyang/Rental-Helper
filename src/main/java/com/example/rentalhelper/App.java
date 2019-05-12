68ad269991cb222c1b962bda7c5e3cb089c4b26d
Po-Yu Yang <corupted5354.cv99@g2.nctu.edu.tw>
2019-05-12 08:15:14 -0400

    refactor code

--- src/main/java/com/example/rentalhelper/App.java
+++ src/main/java/com/example/rentalhelper/App.java
@@ -23,34 +23,17 @@ public class App {
 		System.out.println("資料無效!");
 	}
 
-	public static String getHouseInfo(String id, float area, String type, int price, String owner, String address) {
+	public static String getHouseInfo(int id, float area, String type, int price, String owner, String address) {
 		return id + "物件 - " + area + "坪 - " + type + " - 每月" + price + "元 - 屋主: " + owner + " - 地址: " + address;
 	}
 
 	public static void main(String[] args) {
-		// A
-		float areaA = 10f;
-		String typeA = "套房";
-		int priceA = 12000;
-		String ownerA = "王先生";
-		String addressA = "台北市文山區木柵路一段101號";
-		// B
-		float areaB = 8f;
-		String typeB = "套房";
-		int priceB = 8000;
-		String ownerB = "陳先生";
-		String addressB = "台北市中山區中山路一段102號";
-		// C
-		float areaC = 4f;
-		String typeC = "雅房";
-		int priceC = 6000;
-		String ownerC = "林先生";
-		String addressC = "台北市大安區仁愛路一段102號";
 
-		// All info
-		String infoA = getHouseInfo("A", areaA, typeA, priceA, ownerA, addressA);
-		String infoB = getHouseInfo("B", areaB, typeB, priceB, ownerB, addressB);
-		String infoC = getHouseInfo("C", areaC, typeC, priceC, ownerC, addressC);
+		float[] areas = new float[] { 10f, 8f, 4f };
+		String[] types = new String[] { "套房", "套房", "雅房" };
+		int[] prices = new int[] { 12000, 8000, 6000 };
+		String[] owners = new String[] { "王先生", "陳先生", "林先生" };
+		String[] addresses = new String[] { "台北市文山區木柵路一段101號", "台北市中山區中山路一段102號", "台北市大安區仁愛路一段102號" };
 
 		Scanner sc = new Scanner(System.in);
 
@@ -63,77 +46,64 @@ public class App {
 
 				switch (option) {
 				case 1:
-					System.out.println(infoA);
-					System.out.println(infoB);
-					System.out.println(infoC);
+					for (int i = 0; i < areas.length; i++) {
+						System.out.println(getHouseInfo(i, areas[i], types[i], prices[i], owners[i], addresses[i]));
+					}
+
 					break;
 
 				case 2: {
 					printHouseQuery();
-					String id = sc.next();
-					switch (id) {
-					case "A":
-						System.out.println(infoA);
-						break;
-					case "B":
-						System.out.println(infoB);
-						break;
-					case "C":
-						System.out.println(infoC);
-						break;
-					default:
-						printHouseNotFound();
-						break;
-					}
-					break;
-				}
-				case 3: {
-					printHouseQuery();
-					String id = sc.next();
-					int monthlyPrice = 0;
-					switch (id) {
-					case "A":
-						monthlyPrice = priceA;
-						break;
-					case "B":
-						monthlyPrice = priceB;
-						break;
-					case "C":
-						monthlyPrice = priceC;
-						break;
-					default:
+					int index = sc.nextInt();
+
+					if (!(index >= 0 && index < areas.length)) {
 						printHouseNotFound();
 						continue;
 					}
+
+					System.out.println(getHouseInfo(index, areas[index], types[index], prices[index], owners[index],
+							addresses[index]));
+
+					break;
+				}
+
+				case 3: {
+					printHouseQuery();
+					int index = sc.nextInt();
+					if (!(index >= 0 && index < areas.length)) {
+						printHouseNotFound();
+						continue;
+					}
+					int monthlyPrice = prices[index];
 
 					// 接收合約租期
 					System.out.println("請輸入合約租期(月):");
 					int expectedMonths = sc.nextInt();
 					if (expectedMonths < 0) {
 						printInvalidData();
-					} else {
-						// 接收押金期數
-						System.out.println("請輸入押金期數(月):");
-						int depositMonths = sc.nextInt();
-						if (depositMonths < 0) {
-							printInvalidData();
-						} else {
-							// 接收實際租期
-							System.out.println("請輸入實際租期(月):");
-							int actualMonths = sc.nextInt();
-							if (actualMonths > expectedMonths || actualMonths < 0) {
-								printInvalidData();
-							} else {
-								// 進行租金試算
-								int total = monthlyPrice * actualMonths
-										+ ((actualMonths < expectedMonths) ? (depositMonths * monthlyPrice) : 0);
-								float average = total / (float) actualMonths;
-
-								System.out.println("租金總額: " + total + "元");
-								System.out.println("月平均租金: " + average + "元");
-							}
-						}
+						continue;
 					}
+					// 接收押金期數
+					System.out.println("請輸入押金期數(月):");
+					int depositMonths = sc.nextInt();
+					if (depositMonths < 0) {
+						printInvalidData();
+						continue;
+					}
+					// 接收實際租期
+					System.out.println("請輸入實際租期(月):");
+					int actualMonths = sc.nextInt();
+					if (actualMonths > expectedMonths || actualMonths < 0) {
+						printInvalidData();
+						continue;
+					}
+					// 進行租金試算
+					int total = monthlyPrice * actualMonths
+							+ ((actualMonths < expectedMonths) ? (depositMonths * monthlyPrice) : 0);
+					float average = total / (float) actualMonths;
+
+					System.out.println("租金總額: " + total + "元");
+					System.out.println("月平均租金: " + average + "元");
 
 					break;
 				}

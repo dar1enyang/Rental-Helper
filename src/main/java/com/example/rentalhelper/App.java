4c19687113fbb19a7a56ae5cf309f450e1b92bee
Po-Yu Yang <corupted5354.cv99@g2.nctu.edu.tw>
2019-05-12 07:29:15 -0400

    method added

--- src/main/java/com/example/rentalhelper/App.java
+++ src/main/java/com/example/rentalhelper/App.java
@@ -3,6 +3,30 @@ package com.example.rentalhelper;
 import java.util.Scanner;
 
 public class App {
+	public static void printGuide() {
+		System.out.println("Welcome to this rental helper, please select function:");
+		System.out.println("1. List all rental listings");
+		System.out.println("2. Rental Inquiry");
+		System.out.println("3. 租金試算");
+		System.out.println("4. Exit");
+	}
+
+	public static void printHouseQuery() {
+		System.out.println("請輸入物件名稱(A, B, C):");
+	}
+
+	public static void printHouseNotFound() {
+		System.out.println("很抱歉, 物件不存在!");
+	}
+
+	public static void printInvalidData() {
+		System.out.println("資料無效!");
+	}
+
+	public static String getHouseInfo(String id, float area, String type, int price, String owner, String address) {
+		return id + "物件 - " + area + "坪 - " + type + " - 每月" + price + "元 - 屋主: " + owner + " - 地址: " + address;
+	}
+
 	public static void main(String[] args) {
 		// A
 		float areaA = 10f;
@@ -24,21 +48,15 @@ public class App {
 		String addressC = "台北市大安區仁愛路一段102號";
 
 		// All info
-		String infoA = "A listing - " + areaA + "坪 - " + typeA + " - 每月" + priceA + "元 - 屋主: " + ownerA + " - 地址: "
-				+ addressA;
-		String infoB = "B listing - " + areaB + "坪 - " + typeB + " - 每月" + priceB + "元 - 屋主: " + ownerB + " - 地址: "
-				+ addressB;
-		String infoC = "C listing - " + areaC + "坪 - " + typeC + " - 每月" + priceC + "元 - 屋主: " + ownerC + " - 地址: "
-				+ addressC;
+		String infoA = getHouseInfo("A", areaA, typeA, priceA, ownerA, addressA);
+		String infoB = getHouseInfo("B", areaB, typeB, priceB, ownerB, addressB);
+		String infoC = getHouseInfo("C", areaC, typeC, priceC, ownerC, addressC);
 
 		Scanner sc = new Scanner(System.in);
 
 		mainLoop: while (true) {
-			System.out.println("Welcome to this rental helper, please select function:");
-			System.out.println("1. List all rental listings");
-			System.out.println("2. Rental Inquiry");
-			System.out.println("3. 租金試算");
-			System.out.println("4. Exit");
+
+			printGuide();
 
 			if (sc.hasNextInt()) {
 				int option = sc.nextInt();
@@ -51,7 +69,7 @@ public class App {
 					break;
 
 				case 2: {
-					System.out.println("請輸入物件名稱(A, B, C):");
+					printHouseQuery();
 					String id = sc.next();
 					switch (id) {
 					case "A":
@@ -64,13 +82,13 @@ public class App {
 						System.out.println(infoC);
 						break;
 					default:
-						System.out.println("很抱歉, 物件不存在!");
+						printHouseNotFound();
 						break;
 					}
 					break;
 				}
 				case 3: {
-					System.out.println("請輸入物件名稱(A, B, C):");
+					printHouseQuery();
 					String id = sc.next();
 					int monthlyPrice = 0;
 					switch (id) {
@@ -84,38 +102,39 @@ public class App {
 						monthlyPrice = priceC;
 						break;
 					default:
-						System.out.println("很抱歉, 物件不存在!");
+						printHouseNotFound();
 						continue;
 					}
-					
+
 					// 接收合約租期
 					System.out.println("請輸入合約租期(月):");
 					int expectedMonths = sc.nextInt();
 					if (expectedMonths < 0) {
-						System.out.println("資料無效");
+						printInvalidData();
 					} else {
 						// 接收押金期數
 						System.out.println("請輸入押金期數(月):");
 						int depositMonths = sc.nextInt();
 						if (depositMonths < 0) {
-							System.out.println("資料無效");
+							printInvalidData();
 						} else {
 							// 接收實際租期
 							System.out.println("請輸入實際租期(月):");
 							int actualMonths = sc.nextInt();
 							if (actualMonths > expectedMonths || actualMonths < 0) {
-								System.out.println("資料無效");
+								printInvalidData();
 							} else {
 								// 進行租金試算
-								int total = monthlyPrice * actualMonths + ((actualMonths < expectedMonths) ? (depositMonths * monthlyPrice) : 0);
-								float average = total / (float)actualMonths;
-								
+								int total = monthlyPrice * actualMonths
+										+ ((actualMonths < expectedMonths) ? (depositMonths * monthlyPrice) : 0);
+								float average = total / (float) actualMonths;
+
 								System.out.println("租金總額: " + total + "元");
 								System.out.println("月平均租金: " + average + "元");
 							}
 						}
 					}
-					
+
 					break;
 				}
 				case 4:
